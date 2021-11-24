7160459533830f08d930b8e7ef38758545e377d4
lmasa <luaymasadi@gmail.com>
2021-11-24 20:16:11 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -11,20 +11,20 @@ end
 get '/' do
     # File.read(File.join('app/views', 'index.html'))
     # "hello world"
-finstagram_post = {
- username: "sharky_j",
- avatar_url: "http://naserca.com/images/sharky_j.jpg",
- photo_url: "http://naserca.com/images/shark.jpg",
- time_ago_in_minutes: humanized_time_ago(15),
- like_count: 0,
- comment_count: 1,
- comments: [{
- username: "sharky_j",
- text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
- }]
-}
+    @finstagram_post_shark = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
+    time_ago_in_minutes: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+    username: "sharky_j",
+    text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+    }
 
-finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -37,7 +37,7 @@ finstagram_post_whale = {
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -50,6 +50,10 @@ finstagram_post_whale = {
     }]
   }
 
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  # [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+
+  erb(:index)
   
 end
